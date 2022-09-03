a650c6013768899b3f57454c12b9010d9b1922ff
Nithila Kalaimani <nithilak@illinois.edu>
2022-09-03 17:49:33 +0000

    save for progress

--- mp_stickers/src/Image.cpp
+++ mp_stickers/src/Image.cpp
@@ -7,9 +7,9 @@ void Image::lighten(double amount) {
     errorCheckAmount(amount, 1);
     adjustLuminance(amount);
 }
-// void Image::darken() {
-//    adjustLuminance(-0.1);
-// }
+void Image::darken() {
+   adjustLuminance(-0.1);
+}
 void Image::darken(double amount) { //copied from lighten(double amount), change 2 things
     errorCheckAmount(amount, 1);
     adjustLuminance(-1*amount);
@@ -21,45 +21,49 @@ void Image::saturate(double amount) {
     errorCheckAmount(amount, 1);
     adjustSaturation(amount);
 }
-// void Image::desaturate() {
-//    adjustSaturation(0.1);
-// }
-// void Image::desaturate(double amount) {
-//    errorCheckAmount(amount);
-//    adjustSaturation(amount);
-// }
-// void Image::grayscale() { //copied from adjustLuminance(double amount), then adjusted/slightly modified to fit this function
-//    setHue(0) //no need to error check here, but here is a note just to make sure
-// }
+void Image::desaturate() {
+   adjustSaturation(0.1);
+}
+void Image::desaturate(double amount) {
+   errorCheckAmount(amount, 1);
+   adjustSaturation(amount);
+}
+void Image::grayscale() { //copied from adjustLuminance(double amount), then adjusted/slightly modified to fit this function
+   setHue(0); //no need to error check here, but here is a note just to make sure
+}
 void Image::rotateColor(double degrees) {
     errorCheckAmount(degrees, 360);
     adjustHue(degrees);
 }
-// void Image::illinify() {
+void Image::illinify() {
     //copied from adjustHue(double amount), changed one thing (set elem to 11 or 216 based on closeness)
     //void Image::adjustHue(double amount) { //copied from adjustLuminance, changed l -> h
-    // for (unsigned row = 0; row < height(); row++) {
-    //     for (unsigned col = 0; col < width(); col++) {
-    //         double& elem = getPixel(row, col).h;
-    //         if (elem < 0) {
-    //             throw std::runtime_error("l of pixel " + std::to_string(row) + ", " + std::to_string(col) + " is below 0.");
-    //         }
-    //         if (elem > 1) {
-    //             throw std::runtime_error("l of pixel " + std::to_string(row) + ", " + std::to_string(col) + " is greater than 1.");
-    //         }
-    //         if (elem < 1) {
-    //             if (elem < 113.5) { //should be greater than 0
-    //                 elem = 11;
-    //             } else { //elem >= 113.5, should be less than or equal to 360
-    //                 elem = 216;
-    //             }
-    //         }
-    //     }
-    // }
+    for (unsigned row = 0; row < height(); row++) {
+        for (unsigned col = 0; col < width(); col++) {
+            double& elem = getPixel(row, col).h;
+            if (elem < 0) {
+                throw std::runtime_error("l of pixel " + std::to_string(row) + ", " + std::to_string(col) + " is below 0.");
+            }
+            if (elem > 1) {
+                throw std::runtime_error("l of pixel " + std::to_string(row) + ", " + std::to_string(col) + " is greater than 1.");
+            }
+            if (elem < 1) {
+                if (elem < 113.5) { //should be greater than 0
+                    elem = 11;
+                } else { //elem >= 113.5, should be less than or equal to 360
+                    elem = 216;
+                }
+            }
+        }
+    }
     //}
-// }
-void Image::scale(double factor) {}
-// void Image::scale(unsigned w, unsigned h) {}
+}
+void Image::scale(double factor) {
+    factor = factor + 1;
+}
+void Image::scale(unsigned w, unsigned h) {
+    unsigned num = w * h;
+}
 
 //helpers
 
--- mp_stickers/src/Image.h
+++ mp_stickers/src/Image.h
@@ -12,17 +12,17 @@ class Image: public cs225::PNG {
     public:
     void lighten();
     void lighten(double amount);
-    // void darken();
+    void darken();
     void darken(double amount);
     void saturate();
     void saturate(double amount);
-    // void desaturate();
-    // void desaturate(double amount);
-    // void grayscale();
+    void desaturate();
+    void desaturate(double amount);
+    void grayscale();
     void rotateColor(double degrees);
-    // void illinify();
+    void illinify();
     void scale(double factor);
-    // void scale(unsigned w, unsigned h);
+    void scale(unsigned w, unsigned h);
 
     //helpers
     void adjustLuminance(double amount); //copied from lighten(double amount), change 2 things
