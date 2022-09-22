28ee33d23fc84fef81064fc45114ee2cc04a670f
Nithila Kalaimani <nithilak@illinois.edu>
2022-09-22 14:24:21 +0000

    working on lab_trees

--- mp_clean_lists/entry/main.cpp
+++ mp_clean_lists/entry/main.cpp
@@ -91,99 +91,99 @@ int main() {
   // list4.printv2();
   // std::cout << " expected: {2, 3, 4, 5}" << std::endl;
 
-
+  // // done with this part of the mp at least fot now since it seems to be working correctly
   //  straight from the test case, please help what it going on here
-  const int kListSize = 6;
-  const int kSplitNum = 1;
+  // const int kListSize = 6;
+  // const int kSplitNum = 1;
 
-    List<int> list;
-    for (int i = 0; i < kListSize; i++) {
-        list.insertBack(i);
-    }
-    // std::cout << "\nlist: ";
-    // list.printv2();
-    // std::cout << " expected: {0, 1, 2, 3, 4, 5}" << std::endl;
+  //   List<int> list;
+  //   for (int i = 0; i < kListSize; i++) {
+  //       list.insertBack(i);
+  //   }
+  //   // std::cout << "\nlist: ";
+  //   // list.printv2();
+  //   // std::cout << " expected: {0, 1, 2, 3, 4, 5}" << std::endl;
 
-    stringstream s;
-    list.print(s);  
+  //   stringstream s;
+  //   list.print(s);  
 
-    assert("< 0 1 2 3 4 5 >" == s.str());
+  //   assert("< 0 1 2 3 4 5 >" == s.str());
 
-    stringstream s2;
+  //   stringstream s2;
 
-    // std::cout << "list split at 2" << std::endl;
-    List<int> list4 = list.split(kSplitNum);
+  //   // std::cout << "list split at 2" << std::endl;
+  //   List<int> list4 = list.split(kSplitNum);
 
-    list.print(s2);  
+  //   list.print(s2);  
 
-    list.printv2();
+  //   list.printv2();
 
-    assert("< 0 >" == s2.str());
+  //   assert("< 0 >" == s2.str());
 
-    stringstream s3;
+  //   stringstream s3;
 
-    list4.print(s3);  
+  //   list4.print(s3);  
 
-    list4.printv2();
+  //   list4.printv2();
 
-    assert("< 1 2 3 4 5 >" == s3.str());
+  //   assert("< 1 2 3 4 5 >" == s3.str());
 
-    // list.printv2();
-    // std::cout << " expected: {0, 1}" << std::endl;
-    // list4.printv2();
-    // std::cout << " expected: {2, 3, 4, 5}" << std::endl;
+  //   // list.printv2();
+  //   // std::cout << " expected: {0, 1}" << std::endl;
+  //   // list4.printv2();
+  //   // std::cout << " expected: {2, 3, 4, 5}" << std::endl;
 
-    //copied to test it in main
-    //TEST_CASE("List::split simple", "[weight=5][part=1][valgrind]") {
-    List<int> list3;
+  //   //copied to test it in main
+  //   //TEST_CASE("List::split simple", "[weight=5][part=1][valgrind]") {
+  //   List<int> list3;
 
-    list3.insertBack(1);
-    list3.insertBack(2);
-    list3.insertBack(3);
-    list3.insertBack(4);
+  //   list3.insertBack(1);
+  //   list3.insertBack(2);
+  //   list3.insertBack(3);
+  //   list3.insertBack(4);
 
-    List<int> slist = list3.split(2);
-    stringstream s1, s4;
+  //   List<int> slist = list3.split(2);
+  //   stringstream s1, s4;
 
-    std::cout << "list3" << std::endl;
-    list3.print(s1);
-    list3.printv2();
-    std::cout << "slist" << std::endl;
-    slist.print(s4);
-    slist.printv2();
+  //   std::cout << "list3" << std::endl;
+  //   list3.print(s1);
+  //   list3.printv2();
+  //   std::cout << "slist" << std::endl;
+  //   slist.print(s4);
+  //   slist.printv2();
 
-    assert( "< 1 2 >" == s1.str() );
-    assert( "< 3 4 >" == s4.str() );
-
-
+  //   assert( "< 1 2 >" == s1.str() );
+  //   assert( "< 3 4 >" == s4.str() );
 
 
 
-  //mp part 2
-  // std::cout << "list.reverseNth(3): " << std::endl;
-  // list.reverseNth(3);
-  // list.printv2();
-  // std::cout << " expected: {3, 4, 5, 2, 1, 0}"<< std::endl;
-  // std::cout << "list.reverseNth(4): " << std::endl;
-  // list.reverseNth(4);
-  // list.printv2();
-  // std::cout << " expected: {2, 5, 4, 3, 1, 0}"<< std::endl;
-  // std::cout << "list.reverseNth(2): " << std::endl;
-  // list.reverseNth(2);
-  // list.printv2();
-  // std::cout << " expected: {5, 2, 4, 3, 1, 0}"<< std::endl;
-  // std::cout << "list.reverseNth(5): " << std::endl;
-  // list.reverseNth(2);
-  // list.printv2();
-  // std::cout << " expected (call reverse): {0, 1, 3, 4, 2, 5}" << std::endl;
-  // std::cout << "list.insertBack(6)";
-  // list.insertBack(6);
-  // list.printv2();
-  // std::cout << " expected: {0, 1, 3, 4, 2, 5, 6}" << std::endl;
-  // std::cout << "list.reverseNth(6): " << std::endl;
-  // list.reverseNth(6);
-  // list.printv2();
-  // std::cout << " expected (call reverse): {6, 5, 2, 4, 3, 1, 0}" << std::endl;
+
+
+  // mp part 2
+  std::cout << "list.reverseNth(3): " << std::endl;
+  list.reverseNth(3);
+  list.printv2();
+  std::cout << " expected: {3, 4, 5, 2, 1, 0}"<< std::endl;
+  std::cout << "list.reverseNth(4): " << std::endl;
+  list.reverseNth(4);
+  list.printv2();
+  std::cout << " expected: {2, 5, 4, 3, 1, 0}"<< std::endl;
+  std::cout << "list.reverseNth(2): " << std::endl;
+  list.reverseNth(2);
+  list.printv2();
+  std::cout << " expected: {5, 2, 4, 3, 1, 0}"<< std::endl;
+  std::cout << "list.reverseNth(5): " << std::endl;
+  list.reverseNth(2);
+  list.printv2();
+  std::cout << " expected (call reverse): {0, 1, 3, 4, 2, 5}" << std::endl;
+  std::cout << "list.insertBack(6)";
+  list.insertBack(6);
+  list.printv2();
+  std::cout << " expected: {0, 1, 3, 4, 2, 5, 6}" << std::endl;
+  std::cout << "list.reverseNth(6): " << std::endl;
+  list.reverseNth(6);
+  list.printv2();
+  std::cout << " expected (call reverse): {6, 5, 2, 4, 3, 1, 0}" << std::endl;
 
   return 0;
 }
