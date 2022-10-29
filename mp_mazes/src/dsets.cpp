d581cc218f4fa0201ae2603a38848e37f1a18f84
Nithila Kalaimani <nithilak@illinois.edu>
2022-10-29 03:24:41 +0000

    made/generated starter code from Doxygen for mp_mazes

---
+++ mp_mazes/src/dsets.cpp
@@ -0,0 +1,25 @@
+/* Your code here! */
+#include "dsets.h"
+
+void DisjointSets::addelements(int num) {
+    /**Your code here*/
+}
+
+int DisjointSets::find(int elem) {
+    /**Your code here*/
+    return -1;
+}
+
+void DisjointSets::setunion(int a, int b) {
+    /**Your code here*/
+}
+
+int DisjointSets::size(int elem) {
+    /**Your code here*/
+    return -1;
+}
+
+
+
+
+
---
+++ mp_mazes/src/maze.cpp
@@ -0,0 +1,35 @@
+/* Your code here! */
+#include "maze.h"
+
+SquareMaze::SquareMaze() {
+    /**Your code here*/
+}
+
+void SquareMaze::makeMaze(int width, int height) {
+    /**Your code here*/
+}
+
+bool SquareMaze::canTravel(int x, int y, int dir) const {
+    /**Your code here*/
+    return false;
+}
+
+void SquareMaze::setWall(int x, int y, int dir, bool exists) {
+    /**Your code here*/
+}
+
+
+std::vector<int> SquareMaze::solveMaze() {
+    /**Your code here*/
+}
+
+PNG* SquareMaze::drawMaze() const {
+    /**Your code here*/
+}
+
+
+PNG* SquareMaze::drawMazeWithSolution() {
+    /**Your code here*/
+}
+
+
